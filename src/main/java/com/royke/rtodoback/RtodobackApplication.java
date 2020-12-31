a1eae3d56d6bf1ad7e909a4f8c67a597fc54477a
TheBigR <nevoroy@gmail.com>
2020-12-31 17:17:43 +0200

    initial commit

---
+++ src/main/java/com/royke/rtodoback/RtodobackApplication.java
@@ -0,0 +1,13 @@
+package com.royke.rtodoback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtodobackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtodobackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/royke/rtodoback/RtodobackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.royke.rtodoback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtodobackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
